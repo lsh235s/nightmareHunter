4c4ac1119a4ca090d010a09a4a68489644e83602
lsh235s <lsh235s@naver.com>
2023-04-02 23:48:19 +0900

    몬스터 루트 이동 및 unit 기본 정보 저장 소스 추가

--- Assets/Scripts/GameMoon/Enemy/Enemy.cs
+++ Assets/Scripts/GameMoon/Enemy/Enemy.cs
@@ -8,8 +8,12 @@ public class Enemy : MonoBehaviour
     private float _speed;
     [SerializeField]
     private Rigidbody2D _target;
+    [SerializeField]
+    private GameObject _waypoints;
 
+    List<Transform> _waypointList = new List<Transform>();
     bool isLive;
+    int waypointIndex = 0;
 
     [SerializeField]
     private Animator _animator;
@@ -19,6 +23,12 @@ public class Enemy : MonoBehaviour
     private void Awake() {
         isLive = true;
         _rigidbody = GetComponent<Rigidbody2D>();
+        Transform parentTransform = _waypoints.transform;
+
+        foreach (Transform childTransform in parentTransform)
+        {
+            _waypointList.Add(childTransform);
+        }
     }
 
     private void FixedUpdate() {
@@ -26,12 +36,20 @@ public class Enemy : MonoBehaviour
             return;
 
         _animator.SetFloat("move", 1f);
-        //(방향을 알수 있음)위치 = 타켓 위치 - 나의위치
-       Vector2 dirVec = _target.position - _rigidbody.position;
-       //다음에 가야할 위치 = 방향벡터 * 속도 * 시간
-       Vector2 nextVec = dirVec.normalized  * _speed * Time.fixedDeltaTime;
-       _rigidbody.MovePosition(_rigidbody.position + nextVec);
-       _rigidbody.velocity = Vector2.zero;
+    //     //(방향을 알수 있음)위치 = 타켓 위치 - 나의위치
+    //    Vector2 dirVec = _target.position - _rigidbody.position;
+    //    //다음에 가야할 위치 = 방향벡터 * 속도 * 시간
+    //    Vector2 nextVec = dirVec.normalized  * _speed * Time.fixedDeltaTime;
+    //    _rigidbody.MovePosition(_rigidbody.position + nextVec);
+    //    _rigidbody.velocity = Vector2.zero;
+
+        if(waypointIndex < _waypointList.Count) {
+            transform.position = Vector2.MoveTowards (transform.position, _waypointList[waypointIndex].transform.position, _speed * Time.fixedDeltaTime);
+        
+            if(transform.position == _waypointList[waypointIndex].transform.position) {
+                waypointIndex += 1;
+            }
+        }
     }
 
     private void LateUpdate() {
