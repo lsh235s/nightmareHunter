80b227f888f5f33c1fc3101b1c93085a86ffbb18
lsh235s <lsh235s@naver.com>
2023-07-15 02:16:14 +0900

    무기 드랍 및 샷건 기능 추가

--- Assets/Scripts/Common/Unit/Player/Bullet.cs
+++ Assets/Scripts/Common/Unit/Player/Bullet.cs
@@ -12,29 +12,37 @@ namespace nightmareHunter {
         public float attack;
         public float range; // 사거리
         public Vector3 initialPosition; // 초기 위치
-        //public GameObject bulletDot; // 총알
         public float _bulletSpeed; // 총알 속도
         public GameObject MainObject;
+
+        public GameObject bulletDotAni;
+        public GameObject shotgunAni;
 
         private int targetCount = 0; // 타겟 카운트
         Rigidbody2D rigidbody2D;
 
         private bool activeflag = false;
 
-     
+        private float delayTime = 0.0f;
 
         void Start() {
             rigidbody2D = GetComponent<Rigidbody2D>();
         }
 
         public void setWeaponEffect(int weaponID) {
+            bulletDotAni.SetActive(true);
+            shotgunAni.SetActive(false);
             Debug.Log("weaponID : " + weaponID);
             if(weaponID == 1) {
-                MainObject.transform.Find("bullet_dot").GetComponent<SkeletonAnimation>().AnimationName = "bullet1";
+                bulletDotAni.GetComponent<SkeletonAnimation>().AnimationName = "bullet1";
+            } else if(weaponID == 2) {
+                bulletDotAni.SetActive(false);
+                shotgunAni.SetActive(true);
+                //shotgunAni.GetComponent<Animator>().Play("ShotgunEffect");
             } else if(weaponID == 3) {
-                MainObject.transform.Find("bullet_dot").GetComponent<SkeletonAnimation>().AnimationName =  "bullet1";
+                bulletDotAni.GetComponent<SkeletonAnimation>().AnimationName =  "bullet1";
             } else {
-                MainObject.transform.Find("bullet_dot").GetComponent<SkeletonAnimation>().AnimationName =  "bullet2";
+                bulletDotAni.GetComponent<SkeletonAnimation>().AnimationName =  "bullet2";
             }
         }
 
@@ -50,8 +58,13 @@ namespace nightmareHunter {
 
 
         void Update () {
+            Debug.Log("weaponAttackType : " + weaponAttackType);
             if(weaponAttackType.Equals("DIFFUS")) {
-
+                delayTime += Time.deltaTime;
+                if(delayTime >= 1.0f) {
+                    delayTime = 0.0f;
+                    Destroy(gameObject);
+                }
             } else {
                 if (Vector3.Distance(transform.position, initialPosition) >= range)
                 {
@@ -73,10 +86,14 @@ namespace nightmareHunter {
             yield return null;
         }
 
+
+
+
         private void OnTriggerEnter2D(Collider2D collision) {
             Debug.Log(collision.gameObject.tag);
-            if(collision.gameObject.tag != "Untagged" && collision.gameObject.tag != "Bullet" && collision.gameObject.tag != "Enemy" && collision.gameObject.tag != "Player" && collision.gameObject.tag != "Summon" ) {
-                Debug.Log(collision.gameObject.tag);
+             Debug.Log(weaponType);
+            if(weaponType != 2 && collision.gameObject.tag != "Untagged" && collision.gameObject.tag != "Bullet" && collision.gameObject.tag != "Enemy" && collision.gameObject.tag != "Player" && collision.gameObject.tag != "Summon" ) {
+                Debug.Log("충돌파괴:"+collision.gameObject.tag);
                 Destroy(gameObject);
             }
             if(!activeflag) {
