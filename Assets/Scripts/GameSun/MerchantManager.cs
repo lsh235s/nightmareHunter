d446146209247c307af2a93d6e6e4b6cbb4fdc99
lsh235s <lsh235s@naver.com>
2023-09-19 01:02:00 +0900

    상인 추가

--- Assets/Scripts/GameSun/MerchantManager.cs
+++ Assets/Scripts/GameSun/MerchantManager.cs
@@ -1,26 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Spine.Unity;
 
 namespace nightmareHunter {
     public class MerchantManager : MonoBehaviour
     {
-        public GameObject merchant;
-        // Start is called before the first frame update
-        void Start()
+        public GameObject _merchantPanel;
+        public void OnMouseDown()
         {
-            
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            
-        }
-
-        public void OpenMerchant() {
-            Debug.Log("Open Merchant");
-            merchant.SetActive(true);
+            _merchantPanel.active = true;
         }
     }
 }
