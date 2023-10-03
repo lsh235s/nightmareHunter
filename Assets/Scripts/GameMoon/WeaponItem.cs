584e67c8f761cf45f4a096ace788c6b5983a4da8
lsh235s <lsh235s@naver.com>
2023-10-03 23:09:48 +0900

    무기 이미지 교체를 Spine 방식으로 변경

--- Assets/Scripts/GameMoon/WeaponItem.cs
+++ Assets/Scripts/GameMoon/WeaponItem.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Spine.Unity;
+using Spine;
 
 
 namespace nightmareHunter {
     public class WeaponItem : MonoBehaviour
     {
-        [SerializeField]
-        private Sprite[] weaponSprites;
+
+        private SkeletonMecanim skeletonMecanim;
+        private Animator animator;
+        
         public int weaponType;
-        int weaponImage;
 
         private bool isBlinking = false; // 깜박이는 중인지 여부
 
@@ -18,50 +21,53 @@ namespace nightmareHunter {
         private float elapsedTime = 0f; // 경과한 시간
         private bool increasing = true; // 알파 값 증가 여부
 
+        byte blinkCount = 255; // 깜박이를 위한 수치
+
+
+
         private void Start() {
+            skeletonMecanim = gameObject.GetComponent<SkeletonMecanim>();
+            animator = skeletonMecanim.GetComponent<Animator>();
         }
 
 
         public void SetWeaponType(int type) {
             weaponType = type;
-            weaponImage = type - 1;
 
-            gameObject.GetComponent<SpriteRenderer>().sprite = weaponSprites[weaponImage];
+            animator.SetInteger("GunType", type);
         }
 
         private void Update()
         {
-            float alpha = gameObject.GetComponent<SpriteRenderer>().color.a; 
             elapsedTime += Time.deltaTime; // 경과 시간 증가
             if (elapsedTime >= stateTime)
             {
                 Destroy(gameObject); // 오브젝트 파괴
             } else {
-                if(elapsedTime <= 3) {
+                if(elapsedTime >= 4) {
                     if (increasing)
                     {
-                        alpha += 1f * Time.deltaTime; // 알파 값 증가
-                        if (alpha >= 1f)
+                        blinkCount += 1; // 알파 값 증가
+                        if (blinkCount >= 255)
                         {
-                            alpha = 1f;
+                            blinkCount = 255;
                             increasing = false;
                         }
                     }
                     else
                     {
-                        alpha -= 1f * Time.deltaTime; // 알파 값 감소
-                        if (alpha <= 0f)
+                        blinkCount -= 1; // 알파 값 감소
+                        if (blinkCount <= 0f)
                         {
-                            alpha = 0f;
+                            blinkCount = 0;
                             increasing = true;
                         }
                     }
                 }
-                
 
-                Color newColor = gameObject.GetComponent<SpriteRenderer>().color;
-                newColor.a = alpha;
-                gameObject.GetComponent<SpriteRenderer>().color = newColor;
+                Color endColor = new Color32(255, 255, 255, blinkCount);
+
+                skeletonMecanim.skeleton.SetColor(endColor);
             }
         }
 
